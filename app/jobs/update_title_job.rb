0df97b63f9a03b64dd33946b977ff3f716e948f4
itspanicky <nicky.li92@gmail.com>
2020-11-15 16:27:18 -0500

    Use mechanize to get url title and updates short_url with title

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,7 +1,11 @@
+require "mechanize"
+
 class UpdateTitleJob < ApplicationJob
   queue_as :default
 
   def perform(short_url_id)
-
+    @short_url = ShortUrl.find_by(id: short_url_id)
+    @short_url.title = Mechanize.new.get(@short_url.full_url).title
+    @short_url.save!
   end
 end
