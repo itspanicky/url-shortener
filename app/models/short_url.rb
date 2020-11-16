7450e37ccb255dc543705271d00f5d956dfa76d8
itspanicky <nicky.li92@gmail.com>
2020-11-15 19:26:51 -0500

    Refactor model with validation

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -4,6 +4,7 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
+  validates :full_url, presence: true
   validate :validate_full_url
 
   def short_code
